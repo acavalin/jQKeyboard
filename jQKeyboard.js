d5302f7b89200b693b1715839f674fa2b7e9f297
Alberto Cavalin <acavalin@users.noreply.github.com>
2023-03-18 20:37:09 +0100

    * responsive layout * optimize for B/W ereader devices

--- jQKeyboard.js
+++ jQKeyboard.js
@@ -4,45 +4,45 @@
 var keyboardLayout = [
   // element properties: [text, value, keycode, column_span, is_new_row?]
   [ // NORMAL VIEW
-    ['Close', '27', 27, 2, true], ['1', '1', 49, 0, false], ['2', '2', 50, 0, false], ['3', '3', 51, 0, false],
-    ['4', '4', 52, 0, false], ['5', '5', 53, 0, false], ['6', '6', 54, 0, false], ['7', '7', 55, 0, false],
-    ['8', '8', 56, 0, false], ['9', '9', 57, 0, false], ['0', '0', 48, 0, false], ['BkSpc', '8', 8, 2, false],
+    ['Close', '27', 27, 2,  true], ['1', '1', 49, 0, false], ['2', '2', 50, 0, false], [    '3', '3', 51, 0, false],
+    [    '4',  '4', 52, 0, false], ['5', '5', 53, 0, false], ['6', '6', 54, 0, false], [    '7', '7', 55, 0, false],
+    [    '8',  '8', 56, 0, false], ['9', '9', 57, 0, false], ['0', '0', 48, 0, false], ['BkSpc', '8',  8, 2, false],
     
-    ['q', 'q', 81, 0, true], ['w', 'w', 87, 0, false], ['e', 'e', 69, 0, false], ['r', 'r', 82, 0, false],
-    ['t', 't', 84, 0, false], ['y', 'y', 89, 0, false], ['u', 'u', 85, 0, false], ['i', 'i', 73, 0, false],
-    ['o', 'o', 79, 0, false], ['p', 'p', 80, 0, false], ['[', '[', 219, 0, false], [']', ']', 221, 0, false],
+    [     'q', 'q',  81, 0,  true], ['w', 'w', 87, 0, false], ['e', 'e',  69, 0, false], ['r', 'r',  82, 0, false],
+    [     't', 't',  84, 0, false], ['y', 'y', 89, 0, false], ['u', 'u',  85, 0, false], ['i', 'i',  73, 0, false],
+    [     'o', 'o',  79, 0, false], ['p', 'p', 80, 0, false], ['[', '[', 219, 0, false], [']', ']', 221, 0, false],
     ['&#92;', '\\', 220, 0, false],
     
-    ['a', 'a', 65, 0, true], ['s', 's', 83, 0, false], ['d', 'd', 68, 0, false], ['f', 'f', 70, 0, false],
-    ['g', 'g', 71, 0, false], ['h', 'h', 72, 0, false], ['j', 'j', 74, 0, false], ['k', 'k', 75, 0, false],
+    ['a', 'a', 65, 0,  true], ['s', 's',  83, 0, false], [    'd',  'd',  68, 0, false], [    'f',  'f', 70, 0, false],
+    ['g', 'g', 71, 0, false], ['h', 'h',  72, 0, false], [    'j',  'j',  74, 0, false], [    'k',  'k', 75, 0, false],
     ['l', 'l', 76, 0, false], [';', ';', 186, 0, false], ['&#39;', '\'', 222, 0, false], ['Enter', '13', 13, 3, false],
     
-    ['z', 'z', 90, 0, true], ['x', 'x', 88, 0, false], ['c', 'c', 67, 0, false], ['v', 'v', 86, 0, false],
-    ['b', 'b', 66, 0, false], ['n', 'n', 78, 0, false], ['m', 'm', 77, 0, false], [',', ',', 188, 0, false],
+    ['z', 'z',  90, 0,  true], ['x', 'x',  88, 0, false], ['c', 'c',  67, 0, false], ['v', 'v',  86, 0, false],
+    ['b', 'b',  66, 0, false], ['n', 'n',  78, 0, false], ['m', 'm',  77, 0, false], [',', ',', 188, 0, false],
     ['.', '.', 190, 0, false], ['/', '/', 191, 0, false], ['`', '`', 192, 0, false], ['-', '-', 189, 0, false],
     ['=', '=', 187, 0, false],
     
-    ['Shift', '16', 16, 3, true], ['Space', '32', 32, 12, false], ['Clear', '46', 46, 3, false], ['Shift', '16', 16, 3, false]
+    ['Shift', '16', 16, 3, true], ['Space', '32', 32, 13, false], ['Clear', '46', 46, 3, false], ['Shift', '16', 16, 3, false]
   ], [ // SHIFT VIEW
-    ['Close', '27', 27, 2, true], ['!', '!', 49, 0, false], ['@', '@', 50, 0, false], ['#', '#', 51, 0, false],
-    ['$', '$', 52, 0, false], ['%', '%', 53, 0, false], ['^', '^', 54, 0, false], ['&', '&', 55, 0, false],
-    ['*', '*', 56, 0, false], ['(', '(', 57, 0, false], [')', ')', 48, 0, false], ['BkSpc', '8', 8, 2, false],
+    ['Close', '27', 27, 2,  true], ['!', '!', 49, 0, false], ['@', '@', 50, 0, false], [    '#', '#', 51, 0, false],
+    [    '$',  '$', 52, 0, false], ['%', '%', 53, 0, false], ['^', '^', 54, 0, false], [    '&', '&', 55, 0, false],
+    [    '*',  '*', 56, 0, false], ['(', '(', 57, 0, false], [')', ')', 48, 0, false], ['BkSpc', '8',  8, 2, false],
     
-    ['Q', 'Q', 81, 0, true], ['W', 'W', 87, 0, false], ['E', 'E', 69, 0, false], ['R', 'R', 82, 0, false],
-    ['T', 'T', 84, 0, false], ['Y', 'Y', 89, 0, false], ['U', 'U', 85, 0, false], ['I', 'I', 73, 0, false],
-    ['O', 'O', 79, 0, false], ['P', 'P', 80, 0, false], ['{', '{', 219, 0, false], ['}', '}', 221, 0, false],
+    ['Q', 'Q',  81, 0,  true], ['W', 'W', 87, 0, false], ['E', 'E',  69, 0, false], ['R', 'R',  82, 0, false],
+    ['T', 'T',  84, 0, false], ['Y', 'Y', 89, 0, false], ['U', 'U',  85, 0, false], ['I', 'I',  73, 0, false],
+    ['O', 'O',  79, 0, false], ['P', 'P', 80, 0, false], ['{', '{', 219, 0, false], ['}', '}', 221, 0, false],
     ['|', '|', 220, 0, false],
     
-    ['A', 'A', 65, 0, true], ['S', 'S', 83, 0, false], ['D', 'D', 68, 0, false], ['F', 'F', 70, 0, false],
-    ['G', 'G', 71, 0, false], ['H', 'H', 72, 0, false], ['J', 'J', 74, 0, false], ['K', 'K', 75, 0, false],
+    ['A', 'A', 65, 0,  true], ['S', 'S',  83, 0, false], ['D', 'D',  68, 0, false], [    'F',  'F', 70, 0, false],
+    ['G', 'G', 71, 0, false], ['H', 'H',  72, 0, false], ['J', 'J',  74, 0, false], [    'K',  'K', 75, 0, false],
     ['L', 'L', 76, 0, false], [':', ':', 186, 0, false], ['"', '"', 222, 0, false], ['Enter', '13', 13, 3, false],
     
-    ['Z', 'Z', 90, 0, true], ['X', 'X', 88, 0, false], ['C', 'C', 67, 0, false], ['V', 'V', 86, 0, false],
-    ['B', 'B', 66, 0, false], ['N', 'N', 78, 0, false], ['M', 'M', 77, 0, false], ['<', '<', 188, 0, false],
+    ['Z', 'Z',  90, 0,  true], ['X', 'X',  88, 0, false], ['C', 'C',  67, 0, false], ['V', 'V',  86, 0, false],
+    ['B', 'B',  66, 0, false], ['N', 'N',  78, 0, false], ['M', 'M',  77, 0, false], ['<', '<', 188, 0, false],
     ['>', '>', 190, 0, false], ['?', '?', 191, 0, false], ['~', '~', 192, 0, false], ['_', '_', 189, 0, false],
     ['+', '+', 187, 0, false],
     
-    ['Shift', '16', 16, 3, true], ['Space', '32', 32, 12, false], ['Clear', '46', 46, 3, false], ['Shift', '16', 16, 3, false]
+    ['SHIFT', '16', 16, 3, true], ['Space', '32', 32, 13, false], ['Clear', '46', 46, 3, false], ['SHIFT', '16', 16, 3, false]
   ]
 ];
 
@@ -221,10 +221,7 @@ function onShiftKeyPressed () {
 
   createKeypad(keyboardSet);
   
-  if (keyboardSet === 1)
-    $('button[name="key16"').addClass('shift-active');
-  else
-    $('button[name="key16"').removeClass('shift-active');
+  $('button[name="key16"').toggleClass('shift-active', keyboardSet === 1);
 }//onShiftKeyPressed
 
 /*
